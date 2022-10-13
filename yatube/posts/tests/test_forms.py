2fe675df47caa8942cf7da8ae8b2e2ac21352050
Kovaleva Anastasia <anastasia-yurjevna@yandex.ru>
2022-10-14 01:47:23 +0700

    12

--- yatube/posts/tests/test_forms.py
+++ yatube/posts/tests/test_forms.py
@@ -20,6 +20,11 @@ class PostCreateFormTests(TestCase):
         self.user = User.objects.create_user(username='auth')
         self.authorized_client = Client()
         self.authorized_client.force_login(self.user)
+        self.authorized_client.post(
+            reverse('posts:post_create'),
+            data=form_data,
+            follow=True,
+        )
 
     def test_create_post(self):
         count_posts = Post.objects.count()
@@ -32,15 +37,31 @@ class PostCreateFormTests(TestCase):
             data=form_data,
             follow=True,
         )
-        post_1 = Post.objects.get(id=self.group.pk)
-        author_1 = User.objects.get(username='auth')
-        group_1 = Group.objects.get(title='Заголовок для тестовой группы')
+        self.assertTrue(Post.objects.filter(
+                        text='Данные из формы',
+                        group=self.group.pk,
+                        author=self.user
+                        ).exists())
         self.assertEqual(Post.objects.count(), count_posts + 1)
         self.assertRedirects(response, reverse('posts:profile',
                              kwargs={'username': 'auth'}))
-        self.assertEqual(post_1.text, 'Данные из формы')
-        self.assertEqual(author_1.username, 'auth')
-        self.assertEqual(group_1.title, 'Заголовок для тестовой группы')
+        
+        self.assertEqual(self.user.username, 'auth')
+        self.assertEqual(self.group.title, 'Заголовок для тестовой группы')
+
+    def test_create_post_not_authorized(self):
+        form_data = {
+            'text': 'Данные из формы',
+            'group': self.group.pk
+        }
+        self.guest_client.post(
+            reverse('posts:post_create'),
+            data=form_data,
+            follow=True,
+        )
+        self.assertFalse(Post.objects.filter(
+                         text='Данные из формы'
+                         ).exists())
 
     def test_edit_post(self):
         form_data = {
